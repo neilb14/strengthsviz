512199af7415f6056bcaf73096106dbebc271804
Neil Bourgeois <neil.bourgeois@gmail.com>
2015-09-09 21:30:13 -0600

    initial revision

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    bower_concat: {
+      all: {
+        dest: 'js/bower.js'
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bower-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
